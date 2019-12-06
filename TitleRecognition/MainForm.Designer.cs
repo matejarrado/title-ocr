ac613ebeb9ff3c2f5fc3cd1b5268bef73da1f626
matejarrado <matejarradosavljevic@gmail.com>
2019-12-07 05:12:12 +0800

    initial commit

---
+++ TitleRecognition/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace TitleRecognition
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuEditDetect = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuEditExport = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgImage = new Emgu.CV.UI.ImageBox();
+            this.mnuMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile,
+            this.editToolStripMenuItem});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(800, 24);
+            this.mnuMain.TabIndex = 0;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileOpen});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileOpen.Text = "&Open";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuEditDetect,
+            this.mnuEditExport});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // mnuEditDetect
+            // 
+            this.mnuEditDetect.Name = "mnuEditDetect";
+            this.mnuEditDetect.Size = new System.Drawing.Size(180, 22);
+            this.mnuEditDetect.Text = "&Detect";
+            this.mnuEditDetect.Click += new System.EventHandler(this.mnuEditDetect_Click);
+            // 
+            // mnuEditExport
+            // 
+            this.mnuEditExport.Name = "mnuEditExport";
+            this.mnuEditExport.Size = new System.Drawing.Size(180, 22);
+            this.mnuEditExport.Text = "&Export CSV";
+            this.mnuEditExport.Click += new System.EventHandler(this.mnuEditExport_Click);
+            // 
+            // imgImage
+            // 
+            this.imgImage.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.imgImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgImage.Location = new System.Drawing.Point(0, 24);
+            this.imgImage.Name = "imgImage";
+            this.imgImage.Size = new System.Drawing.Size(800, 426);
+            this.imgImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imgImage.TabIndex = 2;
+            this.imgImage.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.imgImage);
+            this.Controls.Add(this.mnuMain);
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "MainForm";
+            this.Text = "Title Recognition";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imgImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuEditDetect;
+        private System.Windows.Forms.ToolStripMenuItem mnuEditExport;
+        private Emgu.CV.UI.ImageBox imgImage;
+    }
+}
+
---
+++ TitleRecognition/MainForm.cs
@@ -0,0 +1,48 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TitleRecognition
+{
+    public partial class MainForm : Form
+    {
+        public Mat inputMat = null;
+        public List<List<KeyValuePair<RotatedRect, String>>> result = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofn = new OpenFileDialog();
+            ofn.Filter = "Image Files(*.bmp,*.jpg,*.png)|*.bmp;*.jpg;*.png||";
+            if (ofn.ShowDialog() == DialogResult.OK)
+            {
+                inputMat = CvInvoke.Imread(ofn.FileName, ImreadModes.Color);
+                imgImage.Image = inputMat;
+            }
+        }
+
+        private void mnuEditDetect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mnuEditExport_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
